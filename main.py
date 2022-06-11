5a3b9a6125a2303d52bb83130b60d26d9f12ff8c
Ypetrakov <yurapetr19@gmail.com>
2022-06-11 22:55:57 +0300

    Initial commit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import numpy as np
-from generator import generate_tusk
+from generator import Generator
 from Algorithms.greed import greed_alg
 from Algorithms.aco import aco
 from Algorithms.branc_and_bound import branch_and_bound_alg
@@ -17,26 +17,36 @@ def timer_func(func):
     return wrap_func
 
 
-def do_experiment(R, N):
-    average_time = np.zeros(shape=(len(R), 3))
-    average_z = np.zeros(shape=(len(R), 3))
-    i = 0
-    for n in R:
+class Main:
 
-        time_iterations = np.zeros(shape=(3, N))
-        z_iterations = np.zeros(shape=(3, N))
-        for j in range(0, N):
-            k, a, c = generate_tusk(n)
-            z_iterations[0][j], time_iterations[0][j] = timer_func(greed_alg)()
-            z_iterations[1][j], time_iterations[1][j] = timer_func(branch_and_bound_alg)()
-            z_iterations[2][j], time_iterations[2][j] = timer_func(aco)()
-        average_time[i] = [mean(time_iterations[0]), mean(time_iterations[0]), mean(time_iterations[0])]
-        average_z[i] = [mean(z_iterations[0]), mean(z_iterations[0]), mean(z_iterations[0])]
-        i += 1
-    print(average_time)
-    print(average_z)
-    return 1
+    def __init__(self, initial_data_file):
+
+        with open(initial_data_file) as f:
+            self.R = list(map(int, f.readline().split(', ')))
+            self.N = int(f.readline())
+            self.average_time = np.zeros(shape=(len(self.R), 3))
+            self.average_z = np.zeros(shape=(len(self.R), 3))
+
+    def start_experiment(self):
+        i = 0
+        for n in self.R:
+            time_iterations = np.zeros(shape=(3, self.N))
+            z_iterations = np.zeros(shape=(3, self.N))
+            for j in range(0, self.N):
+                k, a, c = Generator.generate_tusk(n)
+                z_iterations[0][j], time_iterations[0][j] = timer_func(greed_alg)()
+                z_iterations[1][j], time_iterations[1][j] = timer_func(branch_and_bound_alg)()
+                z_iterations[2][j], time_iterations[2][j] = timer_func(aco)()
+            self.average_time[i] = [mean(time_iterations[0]), mean(time_iterations[0]), mean(time_iterations[0])]
+            self.average_z[i] = [mean(z_iterations[0]), mean(z_iterations[0]), mean(z_iterations[0])]
+            i += 1
+
+    def print_result(self):
+        print(self.average_time)
+        print(self.average_z)
 
 
 if __name__ == '__main__':
-    do_experiment([5, 10, 20, 30, 40, 50], 10)
+    experiment = Main("initial_data")
+    experiment.start_experiment()
+    experiment.print_result()
